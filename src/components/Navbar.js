b9f1a1ca18bd0acd4c297ff85cc2d254c07de2dd
Edison611 <edisony611@gmail.com>
2022-12-17 03:24:02 -0500

    FINAL

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="nav">
-            <div className="logo"><Link to="/Home">Santa's Time Tracker</Link></div>
+            <div className="logo"><Link to="/Home">Santa's Time Tracker - Begin Here</Link></div>
             <ul className="nav_links">
                 <li><Link to="/Add">Add</Link></li>
                 <li><Link to="/Tasks">Tasks</Link></li>
